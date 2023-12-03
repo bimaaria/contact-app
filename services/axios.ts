d7681225f54fd7889bfe8a912a92e6650ca46b62
Bima Aria Radjasa <bima.aria.radjasa@gmail.com>
2023-12-03 16:41:08 +0700

    change request timeout duration

--- services/axios.ts
+++ services/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseRequest = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api`,
-  timeout: 1000,
+  timeout: 30000,
   headers: {
     "Content-Type": "application/json",
   },
